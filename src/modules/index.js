fd174c1fc2a15abc322436ee1bf06ad650b00f54
Nuno <nunohora@gmail.com>
2016-05-31 15:21:56 +0100

    removed auth0-lock dependency

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,9 +1,7 @@
-import lock from './lock'
 import restaurant from './restaurant'
 import restaurantList from './restaurantList'
 
 export default {
-    lock,
     restaurant,
     restaurantList
 }
