0a16ba79f0bac6bd3eb8cfa0a0647be8de8d11ee
Nuno <nunohora@gmail.com>
2016-06-18 17:42:33 +0100

    improved translations

--- src/translations/en.js
+++ src/translations/en.js
@@ -1,12 +1,15 @@
 export default {
-    "TENS_FOME": "Are you hungry?!",
+    "TENS_FOME": "Hungry?!",
     "COME": "Eat!",
     "ONDE_ESTA": "1. Tell us where you are",
     "PRECISAMOS_CODIGO_POSTAL": "We just need your postcode.",
-    "ESCOLHA_TAKEAWAY": "2. Pick a takeaway",
-    "APETECE_COMER": "What do you feel like eating?",
-    "DINHEIRO_OU_CARTAO": "3. Cash or card",
+    "APETECE_COMER": "2. Pick a takeaway",
+    "EXPLORA_LISTA": "Browse the list of restaurants that deliver to your area",
+    "DINHEIRO_OU_CARTAO": "3. Cash or Card",
     "ACEITAMOS_PAGAMENTO": "We accept both ways of payment.",
-    "JA_ESTA": "4. Done",
-    "COMIDA_CAMINHO": "Your food is on it's way!"
+    "JA_ESTA": "4. All done",
+    "COMIDA_CAMINHO": "You can relax now, your food is being cooked and soon it will be on your way!",
+    "ENCOMENDE_ONLINE": "Order food online",
+    "PROCURE_PERTO_SI": "Find takeaways close to you",
+    "CODIGO_POSTAL_INVALIDO": "Invalid postcode"
 }
--- src/translations/pt.js
+++ src/translations/pt.js
@@ -1,12 +1,15 @@
 export default {
     "TENS_FOME": "Tens fome?!",
     "COME": "Come!",
-    "ONDE_ESTA": "1. Diga-nos onde está",
-    "PRECISAMOS_CODIGO_POSTAL": "Só precisamos do seu código postal.",
-    "ESCOLHA_TAKEAWAY": "2. Escolha um takeaway",
-    "APETECE_COMER": "O que lhe apetece comer?",
+    "ONDE_ESTA": "1. Diz-nos onde estás",
+    "PRECISAMOS_CODIGO_POSTAL": "Introduz o teu código postal completo na caixa de pesquisa e clica em procurar.",
+    "APETECE_COMER": "2. O que te apetece comer?",
+    "EXPLORA_LISTA": "Explora a lista de restaurantes que aceitam pedidos para a tua zona",
     "DINHEIRO_OU_CARTAO": "3. Dinheiro ou cartão",
-    "ACEITAMOS_PAGAMENTO": "Aceitamos as duas formas de pagamento.",
-    "JA_ESTA": "4. Já está",
-    "COMIDA_CAMINHO": "A sua comida está a caminho!"
+    "ACEITAMOS_PAGAMENTO": "Paga como te der mais jeito. Com cartão no checkout ou com dinheiro na altura da entrega.",
+    "PEDIDO_EFECTUADO": "4. Pedido efectuado",
+    "COMIDA_CAMINHO": "Agora podes relaxar, o teu pedido está a ser preparado e em breve estará a caminho!",
+    "ENCOMENDE_ONLINE": "Encomenda comida online",
+    "PROCURE_PERTO_SI": "Procure for restaurantes perto de si",
+    "CODIGO_POSTAL_INVALIDO": "Código postal inválido"
 }
